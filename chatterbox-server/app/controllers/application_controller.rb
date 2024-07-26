a485c1e6cb049be79216f6d4e5a3a2c68cdb557c
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-07-26 11:26:05 -0400

    Create all api endpoints to pass tests

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,27 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get "/messages" do
+    Message.all.order("created_at").to_json
+  end
   
+  post "/messages" do
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  patch "/messages/:id" do
+    message = Message.find(params[:id])
+    message.update(body: params[:body])
+    message.to_json
+  end
+
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
 end
